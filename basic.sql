e4bba9b0ec1588f59563dd90cf704a6ffdb80260
Sumit22545 <sumitmangal05@gmail.com>
2025-12-18 15:53:18 +0530

    basic.sql

---
+++ basic.sql
@@ -0,0 +1,40 @@
+
+show databases;
+use mysql;
+
+create database regex_14;
+show databases;
+
+use regex_14;
+
+-- to see output select statment
+select database();
+
+show tables; -- display tables
+
+
+create table priyanshu(salary int,age int);
+show tables;
+
+-- display information of a table
+describe sumit;
+
+/*
+-- to see databases
+show databases;
+
+-- to print current_database;
+select database();
+
+-- to work with database
+use database;
+
+-- to see tables
+show tables;
+
+--create table
+create table table_name(column datatype)
+
+--table(description) -- to see the column name and datatype
+describe table_name;
+
